1608df14aa01797b76d8ee119d2715facfc9f87d
P1v0ta1 <126491482+P1v0ta1@users.noreply.github.com>
2025-03-07 09:47:45 +0800

    3/7morning

---
+++ HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import HomeSwiper from '@/components/HomeSwiper.vue';
+import { showToast } from 'vant';
+import {ref} from 'vue';
+
+const search= ref('');
+
+const onSearch = function(){
+  showToast(search.value);
+}
+
+const onCancel=function(){
+  showToast('cancel');
+}
+</script>
+
+<template>
+  <van-search
+   v-model="search"
+  show-action 
+  placeholder="pupup"
+  @search="onSearch"
+  @cancel="onCancel"
+  />
+
+<div>{{ search }} </div>
+
+<HomeSwiper />
+
+
+  <VanToast />
+</template>
+
+<style lang="scss" scoped>
+
+</style>
