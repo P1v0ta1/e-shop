25b43601ddf55d5b8b2e2d0604104257f310a6ed
p1votal <2531220201@qq.com>
2025-03-07 10:41:24 +0800

    3/7morning

--- proj1/src/components/HomeSwiper.vue
+++ proj1/src/components/HomeSwiper.vue
@@ -1,29 +1,28 @@
-<script setup>    
-
-
+<script setup>
+// 轮播图内容
+const banner = [
+    '/images/banner1.jpg',
+    '/images/banner2.jpg'
+]
 </script>
 
-
-<template>  
-
-<van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-  <van-swipe-item>1</van-swipe-item>
-  <van-swipe-item>2</van-swipe-item>
-  <van-swipe-item>3</van-swipe-item>
-  <van-swipe-item>4</van-swipe-item>
-</van-swipe>
-
-
+<template>
+    <van-swipe
+        class="home-swiper"
+        :autoplay="3000"
+        indicator-color="var(--van-primary-color)"
+    >
+        <van-swipe-item v-for="item in banner" :key="item">
+            <img :src="item" />
+        </van-swipe-item>
+    </van-swipe>
 </template>
 
-<style lang="scss" scoped>  
-
-.my-swipe .van-swipe-item {
-    color: #fff;
-    font-size: 20px;
-    line-height: 150px;
-    text-align: center;
-    background-color: #39a9ed;
-  }
-
+<style lang="scss" scoped>
+.home-swiper {
+    width: 100%;
+    img {
+        width: 100%;
+    }
+}
 </style>
