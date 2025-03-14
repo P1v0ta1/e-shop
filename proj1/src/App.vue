ef1107af40983beb698a9f64865b371e28dbc994
p1v0tal <2531220201@qq.com>
2025-03-14 11:36:05 +0800

    3/14morning

--- proj1/src/App.vue
+++ proj1/src/App.vue
@@ -1,8 +1,34 @@
 <script setup>
+
+import { useAttrs } from 'vue';
 import TabBar from './components/Tabbar.vue'
+import { showToast } from 'vant';
+import { useRouter } from 'vue-router';
+
+
+const router = useRouter()
+
+//按钮返回函数
+const onClickLeft = () => {
+    if(history.length > 1) {
+      router.back();
+    } else {
+      router.push({ name:'home' });
+    }
+}
+
 </script>
 
 <template>
+  <!-- 参考https://vant-ui.github.io/vant/#/zh-CN/nav-bar#fan-hui-shang-ji -->
+<van-nav-bar
+  :title="$route.meta.title"
+  :left-arrow="$route.meta.isShowBack"
+  @click-left="onClickLeft"
+  fixed
+  placeholder
+/>
+
   <RouterView />
   <TabBar />
 </template>
--- proj1/src/views/MessageView.vue
+++ proj1/src/views/MessageView.vue
@@ -1,11 +1,65 @@
 <script setup>
 
+const messagelist = [
+        {id: 1,
+        icon:"/images/heishou.jpg" ,
+        title:"神鹰哥",
+        value:"3分钟前",
+        label:"知不知道什么叫黑手",},
+        {id: 2,
+        icon:"/images/sunge.jpg" ,
+        title:"孙笑川",
+        value:"15分钟前",
+        label:"你吼那么大声干什么辣",},
+        {id: 3,
+        icon:"/images/maomei.jpg" ,
+        title:"Не переводите этоne",
+        value:"1分钟前",
+        label:"请为我们的队伍投票",},
+        {id: 4,
+        icon:"/images/heishou.jpg" ,
+        title:"神鹰哥",
+        value:"3分钟前",
+        label:"知不知道什么叫黑手",},
+        {id: 5,
+        icon:"/images/heishou.jpg" ,
+        title:"神鹰哥",
+        value:"3分钟前",
+        label:"知不知道什么叫黑手",},
+];
+
 </script>
 
+
 <template>
-    message View
+
+   
+    <van-cell-group>  
+    <!--参考 https://vant-ui.github.io/vant/#/zh-CN/cell#ji-chu-yong-fa -->
+        <van-cell v-for="item in messagelist":key="item.id"
+        :icon="item.icon" 
+        :title="item.title"
+        :value="item.value"
+        :label="item.label"
+        center
+        />
+    </van-cell-group>
+    
 </template>
 
 <style lang="scss" scoped>
 
+:deep(.van-cell) {
+    .van-cell__left-icon{
+        width:40px;
+        height: 40px;
+        margin-right: 10px;
+        .van-icon__image{
+            width:100%;
+            height: 100%;
+        }
+    }
+}
+
 </style>
+
